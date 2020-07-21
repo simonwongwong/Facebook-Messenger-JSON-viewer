567ad057a463a46445b8f40fe7544db01ba64bd1
Simon Wong <simon.wong@uwaterloo.ca>
2020-07-21 18:52:40 -0400

    working prototype

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # Facebook-Messenger-JSON-viewer
-If you've downloaded your Facebook data in JSON format, this tool lets you read the chats more intuitively
+If you've downloaded your Facebook data in JSON format, this tool lets you read the chats more intuitively.
+
+Just open the HTML, select the JSON file, select which participant you are (so that your bubbles are blue) and press the submit button.
+
+Voilà, your chats can now be easily read.
+
+![screenshot](screenshot.png)
+
+## Ideas / TODO
+* Do some advanced searching/filtering on the chat data in JSON format using my [Facebook Messenger tool](https://github.com/simonwongwong/Facebook-Messenger-Statistics/) and then view the messages using this viewer
+* Add a slider or calendar picker to filter dates in the chat (not supported by Facebook Messenger unless you want to spend hours scrolling and loading).
+* Add tooltip to show timestamps of messages
+* Functionality for viewing images/files sent in chat
+
+## Bugs
+* text decoding symbols and emojis currently not working
---
+++ chat_bubbles.js
@@ -0,0 +1,122 @@
+const e = React.createElement;
+var myName;
+var cleanedData;
+
+const fileSelector = document.getElementById('fileupload');
+fileSelector.addEventListener('change', (event) => {
+  const fileList = event.target.files;
+  console.log(fileList);
+  readJSON(fileList[0]);
+});
+
+const domButton = document.querySelector('#submit-button');
+ReactDOM.render(
+  e('button', {onClick: () => handleSubmit()}, "Submit"),
+  domButton
+);
+
+
+function handleSubmit(){
+  ReactDOM.render(e(ChatArea), document.querySelector('#chat-area'))
+  const domChat = document.querySelector('#chat-display');
+  ReactDOM.render(e(ChatBubble, cleanedData.msgs), domChat);
+}
+
+function readJSON(file) {
+  const reader = new FileReader();
+
+  reader.addEventListener('load', (event) => {
+    console.log("JSON load sucessful");
+    msgObject = JSON.parse(event.target.result);
+    cleanedData = cleanData(msgObject);
+    promptParticipantRadio(cleanedData.participants);
+  });
+
+  reader.readAsText(file);
+
+  
+}
+
+function cleanData(raw) {
+  var participants = raw.participants.map(person => person.name);
+  var title = raw.title;
+  var msgs = raw.messages.reverse();
+  var cleaned = {
+    "participants": participants,
+    "title": title,
+    "msgs": msgs,
+  }
+
+  return cleaned
+}
+
+function promptParticipantRadio(participants){
+  const participantsRadio = document.querySelector('#participants-radio');
+  var radioElements = []
+
+  radioElements.push(e('p', {}, 'Which participant are you?'))
+
+  participants.forEach(function (n, i) {
+    radioElements.push(
+      e('input', {
+        type: 'radio', 
+        name: 'participant', 
+        onClick: () => radioClick(n), 
+        id: ("radio-button-" + i)
+      })
+    );
+    radioElements.push(
+      e('label', {}, n)
+    );
+    radioElements.push(e('br'));
+  });
+  ReactDOM.render(radioElements, 
+                  participantsRadio);
+        
+  document.querySelector("#radio-button-0").click();
+
+}
+
+function radioClick(name) {
+  myName = name;
+  console.log(`Setting ${name} as blue bubble`);
+}
+
+class ChatBubble extends React.Component {
+  generateBubbles(msg) {
+    if (msg.sender_name == myName) {
+      return (
+        e(
+          'div', {className: "message-container"}, 
+          e('div', {className: "name-right"}, msg.sender_name),
+          e('div', {className: "bubble-right"}, msg.content)
+        )
+      );
+    } else {
+      return (
+        e(
+          'div', {className: "message-container"}, 
+          e('div', {className: "name-left"}, msg.sender_name),
+          e('div', {className: "bubble-left"}, msg.content)
+        )
+      );
+    }
+
+  }
+  render() {
+    return (
+      cleanedData.msgs.map(msg => this.generateBubbles(msg))
+      )
+  }
+}
+
+
+class ChatArea extends React.Component {
+  render() {
+    return (
+      e('div', {id: "chat-display"}, null)
+    );
+      
+  }
+}
+
